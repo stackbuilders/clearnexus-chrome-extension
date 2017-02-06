67bc5c7825fe6c9a41d7daf1478683f629495f71
Nathan Lander <nlander@stackbuilders.com>
2017-02-06 16:45:38 -0500

    Define API work in progress 1.

--- client-gen/app/Main.hs
+++ client-gen/app/Main.hs
@@ -1,6 +1,39 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeOperators #-}
+
 module Main where
 
 import Lib
+import Data.Text ( Text )
+import Net.Types ( IPv4 )
+
+type PostLinksR =
+  QueryParam "access_token" Token :>
+  "api" :>
+  "links" :>
+  ReqBody '[JSON] CreateLinkData :>
+  Post '[JSON] LinkData
+
+newtype Token = Token {unToken :: Text}
+
+newtype CreateLinkData =
+  CreateLinkData {cldTargetEmail :: !Text}
+
+data LinkData
+  = LinkData { ldEmail :: !Text
+             , ldOrganization :: !Text
+             , ldToken :: !Token
+             , ldUnsubsciptionLink :: !Text
+             , ldSubscriptionLink :: !Text
+             , ldCreatedAt :: !UTCTime
+             , ldClickEvents :: ![ClickEventData] }
+
+data ClickEventData
+  = ClickEventData { cedSubscribed :: !Bool
+                   , cedTime :: !UTCTime
+                   , cedIp :: !(Maybe IPv4)
+                   , cedUserAgent :: !Maybe Text}
 
 main :: IO ()
 main = someFunc
