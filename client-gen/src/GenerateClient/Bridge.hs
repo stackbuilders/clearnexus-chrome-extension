0ee74360a85a7113d07bd81bf6e401009936e6da
Nathan Lander <nlander@stackbuilders.com>
2017-02-08 11:41:51 -0500

    Add PureScript Bridge.

---
+++ client-gen/src/GenerateClient/Bridge.hs
@@ -0,0 +1,18 @@
+module GenerateClient.Bridge where
+
+import GenerateClient.Types
+import GenerateClient.API
+
+data MyBridge
+
+myBridgeProxy :: Proxy MyBridge
+myBridgeProxy = Proxy
+
+myApiProxy :: Proxy GetEmailPropertiesR
+myApiProxy = Proxy
+
+myTypes :: [HaskellType]
+myTypes = [ mkTypeInfo ( Proxy :: Proxy EmailProperties )
+          , mkTypeInfo ( Proxy :: Proxy Token )
+          , mkTypeInfo ( Proxy :: Proxy UriEmail )
+          ]
