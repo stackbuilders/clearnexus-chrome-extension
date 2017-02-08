1878d0fa7f375c2ff43caf941686fb2f2e563991
Nathan Lander <nlander@stackbuilders.com>
2017-02-08 12:50:31 -0500

    Add code to write PureScript code.

--- client-gen/app/Main.hs
+++ client-gen/app/Main.hs
@@ -5,4 +5,8 @@ import System.Environment ( lookupEnv )
 main :: IO ()
 main = do
   maybePath <- lookupEnv "PS_CLIENT_DIRECTORY"
-  maybe ( putStrLn "Please set the PS_CLIENT_DIRECTORY environment variable" ) putStrLn maybePath
+  maybe
+    ( putStrLn "Please set the PS_CLIENT_DIRECTORY environment variable" )
+    ( \path' -> let path = path' ) maybePath
+  writeAPIModule path myBridgeProxy myApiProxy
+  writePSTypes path defaultBridge myTypes
