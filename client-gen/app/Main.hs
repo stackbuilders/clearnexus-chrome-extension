34f1b35a6a41290a0a00255d3bfd2584872446ad
Nathan Lander <nlander@stackbuilders.com>
2017-02-08 13:49:11 -0500

    Import Bridge in Main.

--- client-gen/app/Main.hs
+++ client-gen/app/Main.hs
@@ -1,12 +1,15 @@
 module Main where
 
+import GenerateClient.Bridge
 import System.Environment ( lookupEnv )
 
 main :: IO ()
 main = do
   maybePath <- lookupEnv "PS_CLIENT_DIRECTORY"
-  maybe
-    ( putStrLn "Please set the PS_CLIENT_DIRECTORY environment variable" )
-    ( \path' -> let path = path' ) maybePath
-  writeAPIModule path myBridgeProxy myApiProxy
-  writePSTypes path defaultBridge myTypes
+  case maybePath of
+    Nothing ->
+      putStrLn
+        "Please set the PS_CLIENT_DIRECTORY environment variable"
+    Just path -> do
+      writeAPIModule path myBridgeProxy myApiProxy
+      writePSTypes path defaultBridge myTypes
