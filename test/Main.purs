074e5b0d7bbd347a6d6811a4561eb0ec960c499b
Nathan Lander <nlander@stackbuilders.com>
2017-02-10 12:09:15 -0500

    Add staging server setting.

--- test/Main.purs
+++ test/Main.purs
@@ -4,12 +4,18 @@ import Prelude
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 
+makeSettings :: String -> SPSettings_ SPParams_
+makeSettings uri = SPSettings_ $ SPParams_ uri
+
+clearNexusStaging :: String
+clearNexusStaging = "https://staging.clearnex.us"
+
 main = run [ consoleReporter ] do
   describe "Generated Client" do
     describe "getApiEmailByEmail" do
       it "returns false for an email that is not subscribed" do
         let emailAddr = UriEmail "notsubscribed@test.com"
         let userToken = Token "testToken"
-        isSubscribed <- local $ const "stagingURI" $
+        isSubscribed <- local $ makeSettings clearNexusStaging $
           getApiEmailByEmail emailAddr userToken
         isSubscribed.subscribed `shouldBe` false
