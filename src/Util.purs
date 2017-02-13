cd310cd9729766dad2ee1d2e6d3c40b70ab8c77d
Nathan Lander <nlander@stackbuilders.com>
2017-02-13 10:28:29 -0500

    Add dependencies for getSubscriptionStatus.

--- src/Util.purs
+++ src/Util.purs
@@ -1,10 +1,23 @@
 module Util where
 
+import Control.Monad.Aff ( Aff )
+import Control.Monad.Except.Trans ( ExceptT )
+import Control.Monad.Reader.Trans ( ReaderT )
+import GenerateClient.Types ( EmailProperties
+                            , UriEmail
+                            , Token
+                            )
+import Network.HTTP.Affjax ( AJAX )
+import Servant.PureScript.Affjax ( AjaxError )
+import Servant.PureScript.Settings ( SPSettings_ )
 import ServerAPI ( SPParams_(..)
                  , getApiEmailByEmail
                  )
 
-getSubscriptionStatus :: ExceptT AjaxError
+getSubscriptionStatus :: forall eff.
+                         UriEmail
+                      -> Token
+                      -> ExceptT AjaxError
 			   ( ReaderT ( SPSettings_ SPParams_ )
 			     ( Aff ( ajax :: AJAX | eff ) ) )
                                EmailProperties
