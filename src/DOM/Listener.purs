4b54fe8542e255025d7badd7fb4c897848606045
Sebastian Pulido Gomez <spulido@stackbuilders.com>
2017-03-23 10:32:55 -0500

    Refactoring main callback of extension with Aff pattern

--- src/DOM/Listener.purs
+++ src/DOM/Listener.purs
@@ -2,10 +2,13 @@ module DOM.Listener (textAreaListener) where
 
 
 import Prelude
+import Config (Config(..), ChromeEff)
 import Control.Monad.Aff (runAff)
 import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Console (CONSOLE, logShow)
 import Control.Monad.Eff.Timer (TIMER, setTimeout)
+import Control.Monad.Except.Trans (ExceptT(..), runExceptT)
 import DOM (DOM)
 import DOM.Event.EventTarget (addEventListener, eventListener)
 import DOM.Event.Types (Event, EventType(..))
@@ -17,14 +20,11 @@ import Data.Array (head)
 import Data.Either (Either(..))
 import Data.Function.Eff (EffFn1, mkEffFn1, runEffFn1)
 import Data.Maybe (Maybe(..))
-import Data.Semigroup ((<>))
-import Config (ChromeEff)
 import GenerateClient.Types (EmailProperties(..), LinkData(..))
 import Network.HTTP.Affjax (AJAX)
 import Network.HTTP.StatusCode (StatusCode(..))
 import Servant.PureScript.Affjax (AjaxError(..), ErrorDescription(..), errorToString)
-import Util (getSubscriptionStatus, getLink, postNewLink)
-import Config (Config(..))
+import Util (getLink, getSubscriptionStatus, postNewLink)
 
 
 -- << Type EffFn1 is necessary due to compilation issues of callbacks with effects
@@ -49,36 +49,30 @@ reqCallback :: forall eff r .
                    , ajax :: AJAX
                    , chrome :: ChromeEff | eff ) Unit
 reqCallback serverUrl email items = do
-  runAff logShow subsStatusCallback (getSubscriptionStatus serverUrl email items.authtoken)
+  runAff logShow (const $ pure unit) (runExceptT asyncGetEmailProps >>= (runExceptT <<< asyncPostLink))
   pure unit
   where
-    subsStatusCallback (Left ajaxErr@(AjaxError err)) =
+    asyncGetEmailProps = do
+      (EmailProperties props) <- ExceptT $ getSubscriptionStatus serverUrl email items.authtoken
+      (LinkData lData) <- ExceptT $ getLink serverUrl props.link_token items.authtoken
+      let msg = if props.subscribed
+                then "User is subscribed: " <> lData.unsubscription_link
+                else "User is NOT subscribed: " <> lData.unsubscription_link
+      win <- liftEff window
+      liftEff $ alert msg win
+    -- << >> --
+    asyncPostLink (Left ajaxErr@(AjaxError err)) =
       case err.description of
         UnexpectedHTTPStatus obj ->
           if obj.status == StatusCode 404
           then do
-            runAff logShow postLinkCallback (postNewLink serverUrl email items.authtoken)
-            pure unit
-          else logShow $ errorToString ajaxErr
-        _ -> logShow $ errorToString ajaxErr
-    subsStatusCallback (Right (EmailProperties obj)) = do
-      runAff logShow (getLinkCallback obj.subscribed) (getLink serverUrl obj.link_token items.authtoken)
-      pure unit
-    -- << >> --
-    postLinkCallback (Left ajaxErr@(AjaxError err)) = logShow $ errorToString ajaxErr
-    postLinkCallback (Right (LinkData obj)) = do
-      let msg = "Link created for the first time: " <> " " <> obj.unsubscription_link
-      win <- window
-      alert msg win
-    -- << >> --
-    getLinkCallback _ (Left ajaxErr@(AjaxError err)) = logShow $ errorToString ajaxErr
-    getLinkCallback isSubscribed (Right (LinkData obj)) = do
-      let msg = if isSubscribed
-                then "User is subscribed: " <> " " <> obj.unsubscription_link
-                else "User is NOT subscribed: " <> " " <> obj.unsubscription_link
-      win <- window
-      alert msg win
-
+            (LinkData lData) <- ExceptT $ postNewLink serverUrl email items.authtoken
+            let msg = "Link created for the first time: " <> lData.unsubscription_link
+            win <- liftEff window
+            liftEff $ alert msg win
+          else liftEff $ logShow $ errorToString ajaxErr
+        _ -> liftEff $ logShow $ errorToString ajaxErr
+    asyncPostLink _ = pure unit
 
 -- << Listener for events in the <textarea name="to"> element
 textAreaListener :: forall eff .
