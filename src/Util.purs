5f3891b4f52e135c79e24ef82bebb2958507d833
Sebastian Pulido Gomez <spulido@stackbuilders.com>
2017-11-24 16:35:52 -0500

    Updating Util module

--- src/Util.purs
+++ src/Util.purs
@@ -1,7 +1,7 @@
-module Util ( getSubscriptionStatus
+module Util ( getLastMailing
+            , postNewMailing
             , postNewLink
-            , getLink
-            , EPInstances(..)     ) where
+            ) where
 
 
 import Control.Monad.Aff (Aff)
@@ -10,63 +10,52 @@ import Control.Monad.Reader.Trans (ReaderT, runReaderT)
 import Data.Either (Either)
 import Data.Eq (class Eq, eq)
 import Data.Show (class Show, show)
-import GenerateClient.Types (EmailProperties(..), CreateLinkData(..), LinkData)
+import GenerateClient.Types 
 import Network.HTTP.Affjax (AJAX)
 import Prelude (($), (<<<))
 import Prim (String)
 import Servant.PureScript.Affjax (AjaxError)
 import Servant.PureScript.Settings (SPSettings_, defaultSettings)
-import ServerAPI (SPParams_(..), getApiEmailByEmail, getApiLinkByToken, postApiLinks)
+import ServerAPI (SPParams_(..), getApiMailingLastByEmail, postApiMailings, postApiLinks)
 
 
-type AjaxRequest eff a = ExceptT AjaxError
-                                (ReaderT (SPSettings_ SPParams_)
-                                (Aff (ajax :: AJAX | eff))) a
-
-
-newtype EPInstances = EPInstances EmailProperties
-
-
-instance showEPInstances :: Show EPInstances where
-  show (EPInstances (EmailProperties { subscribed })) =
-    show subscribed
-
-
-instance eqEPInstances :: Eq EPInstances where
-  eq (EPInstances (EmailProperties { subscribed: s1 }))
-     (EPInstances (EmailProperties { subscribed: s2 })) = eq s1 s2
+type AjaxRequest eff a =
+  ExceptT AjaxError (ReaderT (SPSettings_ SPParams_) (Aff (ajax :: AJAX | eff))) a
 
 
 makeSettings :: { baseURL :: String } -> SPSettings_ SPParams_
 makeSettings uri = defaultSettings $ SPParams_ uri
 
-getSubscriptionStatus :: forall eff .
-		         String
-                      -> String
-                      -> String
-                      -> Aff (ajax :: AJAX | eff) (Either AjaxError EmailProperties)
-getSubscriptionStatus url email token =
-  (runReaderT <<< runExceptT)
-    (getApiEmailByEmail email token)
-       (makeSettings { baseURL: url })
+getLastMailing :: forall eff . 
+                  String 
+               -> String
+               -> String
+               -> Aff (ajax :: AJAX | eff) (Either AjaxError LastMailingData)
+getLastMailing url email access_token = 
+  (runReaderT <<< runExceptT) (getApiMailingLastByEmail email access_token) (makeSettings { baseURL: url })
 
-getLink :: forall eff .
-           String
-        -> String
-        -> String
-        -> Aff (ajax :: AJAX | eff) (Either AjaxError LinkData)
-getLink url linkToken accessToken =
-  (runReaderT <<< runExceptT)
-    (getApiLinkByToken linkToken accessToken)
-       (makeSettings { baseURL: url })
+postNewMailing :: forall eff .
+                  String
+               -> String
+               -> String
+               -> String
+               -> Aff (ajax :: AJAX | eff) (Either AjaxError MailingData)
+postNewMailing url email desc access_token = 
+  let createMailingData = CreateMailingData { link_email: email, description: desc }
+  in (runReaderT <<< runExceptT) (postApiMailings createMailingData access_token) (makeSettings { baseURL: url })
 
 postNewLink :: forall eff .
                String
             -> String
             -> String
-            -> Aff (ajax :: AJAX | eff) (Either AjaxError LinkData)
-postNewLink url email token =
-  let createLinkData = CreateLinkData { target_email: email }
-  in (runReaderT <<< runExceptT)
-       (postApiLinks createLinkData token)
-         (makeSettings { baseURL: url })
+            -> String
+            -> Aff (ajax :: AJAX | eff) (Either AjaxError MailingData)
+postNewLink url email desc access_token = 
+  let createLinkData = CreateLinkData { target_email: email, description: desc }
+  in (runReaderT <<< runExceptT) (postApiLinks createLinkData access_token) (makeSettings { baseURL: url })
+
+
+
+
+
+
