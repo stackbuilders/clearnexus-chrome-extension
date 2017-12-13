b12b8e08d326548fd9a06e29e175dd144851ee28
Sebastian Pulido <sebastian0092@gmail.com>
2017-12-13 09:27:57 -0500

    Fixing bug for pop alert on saving of token for Mac OS (#18)

--- src/Chrome/Storage.js
+++ src/Chrome/Storage.js
@@ -13,12 +13,23 @@ exports.uncurriedSaveToken = function(document_, chrome_) {
       chrome.storage.sync.set({
 	authtoken: token
       }, function() {
-	alert('Your token has been saved');
-	document.getElementById('save_tkn_cn').disabled = true;
-	input.disabled = true;
+	var options = { type: "basic",
+			iconUrl: "logo.png",
+			title: "Clear Nexus",
+			message: "Your token has been set" };
+
+	chrome.notifications.create("token-notif-id", options, function() {
+	  document.getElementById('save_tkn_cn').disabled = true;
+	  input.disabled = true;
+
+	  // Delay a bit the closing of the pop up for better user experience.
+	  setTimeout(function() {
+	    chrome.notifications.clear("token-notif-id");
+	    window.close();
+	  }, 1700);
+	});
       });
     }
-
     return token;
   };
 };
